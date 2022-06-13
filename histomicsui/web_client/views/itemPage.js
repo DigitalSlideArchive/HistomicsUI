DigitalSlideArchive/HistomicsUI

commit c5a8dd94958d4ac596ec4f1fb5fc5601ad5842e7

Change reference for button location from upload button to 'Actions' menu

histomicsui/web_client/views/itemPage.js
@@ -42,7 +42,7 @@ wrap(ItemView, 'render', function (render) {
                 );
             }
             if (!this.$el.find('.g-hui-open-item[role="button"]').length && this.model.attributes.largeImage) {
-                this.$el.find('.g-upload-into-item').after(
+                this.$el.find('.btn-group').before(
                     `<a class="g-hui-open-item btn btn-sm btn-primary" role="button" href="${webrootPath}#?image=${this.model.id}" target="_blank">
                             <i class="icon-link-ext"></i>Open in ${brandName}
                     </a>`

commit 60937ae5d2704bb4c1576ecd283b5f71dd853787

Remove accidental extra 'Open in HUI' button
Commit c5a8dd9 accidentally created an additional 'Open in Histomics UI'
button at the bottom of the page, so this provides an additional
specifying parent class during button creation to address the issue.

histomicsui/web_client/views/itemPage.js
@@ -42,7 +42,7 @@ wrap(ItemView, 'render', function (render) {
                 );
             }
             if (!this.$el.find('.g-hui-open-item[role="button"]').length && this.model.attributes.largeImage) {
-                this.$el.find('.btn-group').before(
+                this.$el.find('.g-item-header .btn-group').before(
                     `<a class="g-hui-open-item btn btn-sm btn-primary" role="button" href="${webrootPath}#?image=${this.model.id}" target="_blank">
                             <i class="icon-link-ext"></i>Open in ${brandName}
                     </a>`