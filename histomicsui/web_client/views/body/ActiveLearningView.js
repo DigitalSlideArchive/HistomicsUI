a9467979b14abc8ea34404cc4517793aea9bc98f
naglepuff <mike.nagler@kitware.com>
2022-09-13 15:37:40 -0400

    Retrieve necessary data and display images

--- histomicsui/web_client/views/body/ActiveLearningView.js
+++ histomicsui/web_client/views/body/ActiveLearningView.js
@@ -1,10 +1,26 @@
 import View from '../View';
+import { restRequest, getApiRoot } from '@girder/core/rest';
+import _ from 'underscore';
+
+import router from '../../router';
 
 import learningTemplate from '../../templates/body/learning.pug';
 import ActiveLearningContainer from '../../vue/components/ActiveLearning/ActiveLearningContainer.vue';
 
 var ActiveLearningView = View.extend({
     initialize(settings) {
+        router.setQuery(); // ??? don't think I should need to do this, but ok...
+        this.trainingDataFolderId = router.getQuery('folder');
+        this.imageItemsById = {};
+        this.annotationsByImageId = {};
+        this.sortedSuperpixelIndices = [];
+
+        restRequest({
+            url: `folder/${this.trainingDataFolderId}`
+        }).then((response) => {
+            this.annotationBaseName = response.meta['active_learning_annotation_name'];
+            this.getAnnotations();
+        });
         this.render();
     },
 
@@ -12,15 +28,115 @@ var ActiveLearningView = View.extend({
         const el = this.$('.h-active-learning-container').get(0);
         const vm = new ActiveLearningContainer({
             el,
+            propsData: {
+                router: router,
+                trainingDataFolderId: this.trainingDataFolderId,
+                annotationsByImageId: this.annotationsByImageId,
+                annotationBaseName: this.annotationBaseName,
+                sortedSuperpixelIndices: this.sortedSuperpixelIndices,
+                apiRoot: getApiRoot()
+            }
         });
         this.vueApp = vm;
     },
 
     render() {
         this.$el.html(learningTemplate());
-        this.mountVueComponent();
         return this;
     },
+
+    getAnnotations() {
+        console.log('getting annotations...');
+        const annotationsToFetchByImage = {};
+        restRequest({
+            url: 'item',
+            data: {
+                folderId: this.trainingDataFolderId
+            }
+        }).then((response) => {
+            _.forEach(response, (item) => {
+                if (item.largeImage) {
+                    this.imageItemsById[item._id] = item;
+                    this.annotationsByImageId[item._id] = {};
+
+                    restRequest({
+                        url: 'annotation',
+                        data: {
+                            itemId: item._id,
+                            sort: 'created',
+                            sortdir: -1
+                        }
+                    }).then((response) => {
+                        // note: refine name checking
+                        const predictionsAnnotations = _.filter(response, (annotation) => {
+                            return this.annotationIsValid(annotation) && annotation.annotation.name.includes('Predictions');
+                        });
+                        const superpixelAnnotations = _.filter(response, (annotation) => {
+                            return this.annotationIsValid(annotation) && !annotation.annotation.name.includes('Predictions');
+                        });
+                        annotationsToFetchByImage[item._id] = {
+                            predictions: predictionsAnnotations[0]._id,
+                            superpixels: superpixelAnnotations[0]._id
+                        };
+                        this.fetchAnnotations(annotationsToFetchByImage);
+                    });
+                }
+            })
+        });
+    },
+
+    annotationIsValid(annotation) {
+        return true;
+    },
+
+    fetchAnnotations(annotationsToFetchByImage) {
+        const promises = [];
+        // use $.when to resolve trigger creation of vue component
+        _.forEach(Object.keys(annotationsToFetchByImage), (imageId) => {
+            _.forEach(['predictions', 'superpixels'], (key) => {
+                promises.push(restRequest({
+                    url: `annotation/${annotationsToFetchByImage[imageId][key]}`
+                }).done((res) => {
+                    // check validity of annotation
+                    this.annotationsByImageId[imageId][key] = res;
+                }));
+            });
+        });
+        $.when(...promises).then(() => {
+            console.log('retrieved annotations...');
+            this.getSortedSuperpixelIndices();
+            this.mountVueComponent();
+        });
+    },
+
+    getSortedSuperpixelIndices() {
+        console.time("sorting");
+        const superPixelConfidenceData = [];
+        _.forEach(Object.keys(this.annotationsByImageId), (imageId) => {
+            const annotation = this.annotationsByImageId[imageId].predictions;
+            console.log(annotation);
+            const userData = annotation.annotation.elements[0].user;
+            const superpixelImageId = annotation.annotation.elements[0].girderId;
+            const boundaries = annotation.annotation.elements[0].boundaries;
+            const scale = annotation.annotation.elements[0].transform.matrix[0][0];
+            console.log(userData);
+            _.forEach(userData.confidence, (score, index) => {
+                const bbox = userData.bbox.slice(index * 4, index * 4 + 4);
+                superPixelConfidenceData.push({
+                    index: index,
+                    confidence: score,
+                    imageId: imageId,
+                    superpixelImageId: superpixelImageId,
+                    boundaries: boundaries,
+                    scale: scale,
+                    bbox: bbox
+                });
+            });
+        });
+        this.sortedSuperpixelIndices = _.sortBy(superPixelConfidenceData, 'confidence');
+        console.timeEnd("sorting");
+    }
+
 });
 
 export default ActiveLearningView;
--- histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningContainer.vue
+++ histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningContainer.vue
@@ -1,9 +1,78 @@
 <script>
 import Vue from 'vue';
+import { restRequest } from '@girder/core/rest';
+import _ from 'underscore';
 
 export default Vue.extend({
+    props: [
+        'router',
+        'trainingDataFolderId',
+        'annotationsByImageId',
+        'annotationBaseName',
+        'sortedSuperpixelIndices',
+        'apiRoot'
+    ],
+    data() {
+        return {
+            currentImageId: '',
+            imageItemsById: {},
+            annotationsByImage: {},
+            superpixelsToTrain: [],
+        };
+    },
+    computed: {
+        superPixelsToDisplay() {
+            return this.sortedSuperpixelIndices.slice(0, 8);
+        }
+    },
+    methods: {
+        getWsiRegionUrl(superPixel) {
+            const imageId = superPixel.imageId;
+            const bbox = superPixel.bbox;
+            const regionWidth = bbox[2] - bbox[0];
+            const regionHeight = bbox[3] - bbox[1];
+            const scaleFactor = Math.max(regionWidth, regionHeight);
+            const thumbnailWidth = Math.floor(100 * regionWidth / scaleFactor);
+            const thumbnailHeight = Math.floor(100 * regionHeight / scaleFactor);
+            const params = `?left=${bbox[0]}&top=${bbox[1]}&right=${bbox[2]}&bottom=${bbox[3]}&width=${thumbnailWidth}&height=${thumbnailHeight}`;
+            return `${this.apiRoot}/item/${imageId}/tiles/region${params}`;
+        },
+        getSuperpixelRegionUrl(superPixel) {
+            const imageId = superPixel.superpixelImageId;
+            const index = superPixel.index;
+            const pixelVals = superPixel.boundaries ? [index * 2, index * 2 + 1] : [index];
+            const bbox = superPixel.bbox;
+            const scale = superPixel.scale;
+            const regionWidth = bbox[2] - bbox[0];
+            const regionHeight = bbox[3] - bbox[1];
+            const scaleFactor = Math.max(regionWidth, regionHeight);
+            const thumbnailWidth = Math.floor(100 * regionWidth / scaleFactor);
+            const thumbnailHeight = Math.floor(100 * regionHeight / scaleFactor);
+            console.log({ scale });
+            const params = `?left=${bbox[0] / scale}&top=${bbox[1] / scale}&right=${bbox[2] / scale}&bottom=${bbox[3] / scale}&width=${thumbnailWidth}&height=${thumbnailHeight}&encoding=PNG`;
+            const functionJson = JSON.stringify({
+                function: {
+                    name: 'large_image.tilesource.stylefuncs.maskPixelValues',
+                    context: true,
+                    parameters: {
+                        values: pixelVals
+                    }
+                },
+                bands: []
+            });
+            const functionParam = `&style=${encodeURIComponent(functionJson)}`;
+            return `${this.apiRoot}/item/${imageId}/tiles/region${params}${functionParam}`;
+        }
+    },
     mounted() {
-        console.log('mounted active learning vue component');
+        console.log('vue component mounted...');
+        console.log({ props: {
+            a: this.trainingDataFolderId,
+            b: this.annotationsByImageId,
+            c: this.annotationBaseName,
+            d: this.sortedSuperpixelIndices,
+            e: this.apiRoot
+        } });
     }
 });
 </script>
@@ -11,5 +80,31 @@ export default Vue.extend({
 <template>
     <div>
         <span>Active Learning</span>
+        <div
+            v-for="superPixel in superPixelsToDisplay"
+            :key="`${superPixel.imageId}_${superPixel.index}`"
+            class="h-superpixel-container"
+        >
+            <img
+                class="h-superpixel-img h-wsi-region"
+                :src="getWsiRegionUrl(superPixel)"
+            />
+            <img
+                class="h-superpixel-img h-superpixel-region"
+                :src="getSuperpixelRegionUrl(superPixel)"
+            />
+        </div>
     </div>
 </template>
+
+<style scoped>
+.h-superpixel-container {
+    position: relative;
+}
+
+.h-superpixel-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+</style>
