18e72a8d514a2f315be7d0066f6f7e50baad06b4
naglepuff <mike.nagler@kitware.com>
2022-09-14 18:03:56 -0400

    Create filmstrip and card components

--- histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningContainer.vue
+++ histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningContainer.vue
@@ -1,9 +1,13 @@
 <script>
 import Vue from 'vue';
-import { restRequest } from '@girder/core/rest';
 import _ from 'underscore';
 
+import ActiveLearningFilmStrip from './ActiveLearningFilmStrip.vue';
+
 export default Vue.extend({
+    components: {
+        ActiveLearningFilmStrip
+    },
     props: [
         'router',
         'trainingDataFolderId',
@@ -21,58 +25,53 @@ export default Vue.extend({
         };
     },
     computed: {
-        superPixelsToDisplay() {
+        superpixelsToDisplay() {
             return this.sortedSuperpixelIndices.slice(0, 8);
         }
     },
     methods: {
-        getWsiRegionUrl(superPixel) {
-            const imageId = superPixel.imageId;
-            const bbox = superPixel.bbox;
-            const regionWidth = bbox[2] - bbox[0];
-            const regionHeight = bbox[3] - bbox[1];
-            const scaleFactor = Math.max(regionWidth, regionHeight);
-            const thumbnailWidth = Math.floor(100 * regionWidth / scaleFactor);
-            const thumbnailHeight = Math.floor(100 * regionHeight / scaleFactor);
-            const params = `?left=${bbox[0]}&top=${bbox[1]}&right=${bbox[2]}&bottom=${bbox[3]}&width=${thumbnailWidth}&height=${thumbnailHeight}`;
-            return `${this.apiRoot}/item/${imageId}/tiles/region${params}`;
-        },
-        getSuperpixelRegionUrl(superPixel) {
-            const imageId = superPixel.superpixelImageId;
-            const index = superPixel.index;
-            const pixelVals = superPixel.boundaries ? [index * 2, index * 2 + 1] : [index];
-            const bbox = superPixel.bbox;
-            const scale = superPixel.scale;
-            const regionWidth = bbox[2] - bbox[0];
-            const regionHeight = bbox[3] - bbox[1];
-            const scaleFactor = Math.max(regionWidth, regionHeight);
-            const thumbnailWidth = Math.floor(100 * regionWidth / scaleFactor);
-            const thumbnailHeight = Math.floor(100 * regionHeight / scaleFactor);
-            console.log({ scale });
-            const params = `?left=${bbox[0] / scale}&top=${bbox[1] / scale}&right=${bbox[2] / scale}&bottom=${bbox[3] / scale}&width=${thumbnailWidth}&height=${thumbnailHeight}&encoding=PNG`;
-            const functionJson = JSON.stringify({
-                function: {
-                    name: 'large_image.tilesource.stylefuncs.maskPixelValues',
-                    context: true,
-                    parameters: {
-                        values: pixelVals
-                    }
-                },
-                bands: []
-            });
-            const functionParam = `&style=${encodeURIComponent(functionJson)}`;
-            return `${this.apiRoot}/item/${imageId}/tiles/region${params}${functionParam}`;
-        }
+        // getWsiRegionUrl(superPixel) {
+            // const imageId = superPixel.imageId;
+            // const bbox = superPixel.bbox;
+            // const regionWidth = bbox[2] - bbox[0];
+            // const regionHeight = bbox[3] - bbox[1];
+            // const scaleFactor = Math.max(regionWidth, regionHeight);
+            // const thumbnailWidth = Math.floor(100 * regionWidth / scaleFactor);
+            // const thumbnailHeight = Math.floor(100 * regionHeight / scaleFactor);
+            // const params = `?left=${bbox[0]}&top=${bbox[1]}&right=${bbox[2]}&bottom=${bbox[3]}&width=${thumbnailWidth}&height=${thumbnailHeight}`;
+            // return `${this.apiRoot}/item/${imageId}/tiles/region${params}`;
+        // },
+        // getSuperpixelRegionUrl(superPixel) {
+            // const imageId = superPixel.superpixelImageId;
+            // const index = superPixel.index;
+            // const pixelVals = superPixel.boundaries ? [index * 2, index * 2 + 1] : [index];
+            // const bbox = superPixel.bbox;
+            // const scale = superPixel.scale;
+            // const regionWidth = bbox[2] - bbox[0];
+            // const regionHeight = bbox[3] - bbox[1];
+            // const scaleFactor = Math.max(regionWidth, regionHeight);
+            // const thumbnailWidth = Math.floor(100 * regionWidth / scaleFactor);
+            // const thumbnailHeight = Math.floor(100 * regionHeight / scaleFactor);
+            // console.log({ scale });
+            // const params = `?left=${bbox[0] / scale}&top=${bbox[1] / scale}&right=${bbox[2] / scale}&bottom=${bbox[3] / scale}&width=${thumbnailWidth}&height=${thumbnailHeight}&encoding=PNG`;
+            // const functionJson = JSON.stringify({
+                // function: {
+                    // name: 'large_image.tilesource.stylefuncs.maskPixelValues',
+                    // context: true,
+                    // parameters: {
+                        // values: pixelVals,
+                        // positive: [0, 0, 0, 0],
+                        // negative: [255, 255, 255, 255]
+                    // }
+                // },
+                // bands: []
+            // });
+            // const functionParam = `&style=${encodeURIComponent(functionJson)}`;
+            // return `${this.apiRoot}/item/${imageId}/tiles/region${params}${functionParam}`;
+        // }
     },
     mounted() {
         console.log('vue component mounted...');
-        console.log({ props: {
-            a: this.trainingDataFolderId,
-            b: this.annotationsByImageId,
-            c: this.annotationBaseName,
-            d: this.sortedSuperpixelIndices,
-            e: this.apiRoot
-        } });
     }
 });
 </script>
@@ -80,8 +79,8 @@ export default Vue.extend({
 <template>
     <div>
         <span>Active Learning</span>
-        <div
-            v-for="superPixel in superPixelsToDisplay"
+        <!-- <div
+            v-for="superPixel in superpixelsToDisplay"
             :key="`${superPixel.imageId}_${superPixel.index}`"
             class="h-superpixel-container"
         >
@@ -93,18 +92,34 @@ export default Vue.extend({
                 class="h-superpixel-img h-superpixel-region"
                 :src="getSuperpixelRegionUrl(superPixel)"
             />
-        </div>
+        </div> -->
+        <active-learning-film-strip
+            :superpixelsToDisplay="this.superpixelsToDisplay"
+            :apiRoot="this.apiRoot"
+        />
     </div>
 </template>
 
 <style scoped>
 .h-superpixel-container {
     position: relative;
+    height: 100px;
+    width: 100px;
 }
 
-.h-superpixel-img {
+.h-wsi-region {
     position: absolute;
-    top: 0;
-    left: 0;
+    left: 0px;
+    top: 0px;
+    z-index: 10;
+
+}
+
+.h-superpixel-region {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    z-index: 20;
+
 }
 </style>
---
+++ histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningFilmStrip.vue
@@ -0,0 +1,150 @@
+<script>
+import _ from 'underscore';
+
+import ActiveLearningFilmStripCard from './ActiveLearningFilmStripCard.vue';
+
+export default {
+    components: {
+        ActiveLearningFilmStripCard
+    },
+    props: ['superpixelsToDisplay', 'apiRoot'],
+    computed: {
+        headerStyles() {
+            return _.map(this.superpixelsToDisplay, (superpixel) => {
+                console.log(superpixel);
+                return {
+                    'font-size': '80%',
+                    'background-color': superpixel.categories[superpixel.prediction].fillColor
+                }
+            });
+        }
+    },
+    methods: {
+        getWsiRegionUrl(superPixel) {
+            const imageId = superPixel.imageId;
+            const bbox = superPixel.bbox;
+            const regionWidth = bbox[2] - bbox[0];
+            const regionHeight = bbox[3] - bbox[1];
+            const scaleFactor = Math.max(regionWidth, regionHeight);
+            const thumbnailWidth = Math.floor(125 * regionWidth / scaleFactor);
+            const thumbnailHeight = Math.floor(125 * regionHeight / scaleFactor);
+            const params = `?left=${bbox[0]}&top=${bbox[1]}&right=${bbox[2]}&bottom=${bbox[3]}&width=${thumbnailWidth}&height=${thumbnailHeight}`;
+            return `${this.apiRoot}/item/${imageId}/tiles/region${params}`;
+        },
+        getSuperpixelRegionUrl(superPixel) {
+            const imageId = superPixel.superpixelImageId;
+            const index = superPixel.index;
+            const pixelVals = superPixel.boundaries ? [index * 2, index * 2 + 1] : [index];
+            const bbox = superPixel.bbox;
+            const scale = superPixel.scale;
+            const regionWidth = bbox[2] - bbox[0];
+            const regionHeight = bbox[3] - bbox[1];
+            const scaleFactor = Math.max(regionWidth, regionHeight);
+            const thumbnailWidth = Math.floor(125 * regionWidth / scaleFactor);
+            const thumbnailHeight = Math.floor(125 * regionHeight / scaleFactor);
+            const params = `?left=${bbox[0] / scale}&top=${bbox[1] / scale}&right=${bbox[2] / scale}&bottom=${bbox[3] / scale}&width=${thumbnailWidth}&height=${thumbnailHeight}&encoding=PNG`;
+            const functionJson = JSON.stringify({
+                function: {
+                    name: 'large_image.tilesource.stylefuncs.maskPixelValues',
+                    context: true,
+                    parameters: {
+                        values: pixelVals,
+                        positive: [0, 0, 0, 0],
+                        negative: [255, 255, 255, 255]
+                    }
+                },
+                bands: []
+            });
+            const functionParam = `&style=${encodeURIComponent(functionJson)}`;
+            return `${this.apiRoot}/item/${imageId}/tiles/region${params}${functionParam}`;
+        }
+
+    },
+    mounted() {
+        console.log('film strip mounted...');
+        console.log({ apiRoot: this.apiRoot, superPixelsToDisplay: this.superPixelsToDisplay });
+    }
+}
+</script>
+
+<template>
+    <div class="h-filmstrip">
+        <!-- previous button -->
+        <!-- create a component for these, probably -->
+        <active-learning-film-strip-card
+            v-for="superpixel in superpixelsToDisplay"
+            :key="`${superpixel.imageId}_${superpixel.index}`"
+            :superpixel="superpixel"
+            :apiRoot="apiRoot"
+        />
+        <!-- <div
+            v-for="superpixel, index in superpixelsToDisplay"
+            :key="`${superpixel.imageId}_${superpixel.index}`"
+            class="h-superpixel-card"
+        >
+            <div
+                class="h-superpixel-card-header"
+                :style="headerStyles[index]"
+            >
+                Confidence: {{ superpixel.confidence.toFixed(3) }}
+            </div>
+            <div class="h-superpixel-body">
+                <div class="h-superpixel-container">
+                    <img
+                        class="h-superpixel-img h-wsi-region"
+                        :src="getWsiRegionUrl(superpixel)"
+                    />
+                    <img
+                        class="h-superpixel-img h-superpixel-region"
+                        :src="getSuperpixelRegionUrl(superpixel)"
+                    />
+                </div>
+            </div>
+            <div class="h-superpixel-card-footer">
+                <label :for="`${key}_radio`">Agree? </label>
+            </div>
+        </div> -->
+        <!-- next button -->
+    </div>
+</template>
+
+<style scoped>
+.h-filmstrip {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    bottom: 0px;
+    width: 100%;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    background-color: rgba(0, 0, 0, 0.6);
+}
+
+.h-superpixel-card {
+    display: flex;
+    flex-direction: column;
+    column-gap: 0px;
+    background-color: white;
+}
+
+.h-superpixel-container {
+    position: relative;
+    height: 125px;
+    width: 125px;
+}
+
+.h-wsi-region {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    z-index: 10;
+}
+
+.h-superpixel-region {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    z-index: 20;
+}
+</style>
