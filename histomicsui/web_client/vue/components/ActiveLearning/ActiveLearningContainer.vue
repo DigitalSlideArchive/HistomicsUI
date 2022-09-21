8ed1f6145c6001865641f10408fea882bb70306e
naglepuff <mike.nagler@kitware.com>
2022-09-21 15:35:27 -0400

    Center on selected superpixel

--- histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningContainer.vue
+++ histomicsui/web_client/vue/components/ActiveLearning/ActiveLearningContainer.vue
@@ -6,6 +6,8 @@ import { restRequest } from '@girder/core/rest';
 
 import ActiveLearningFilmStrip from './ActiveLearningFilmStrip.vue';
 
+import store from './store.js';
+
 export default Vue.extend({
     components: {
         ActiveLearningFilmStrip
@@ -24,50 +26,85 @@ export default Vue.extend({
             imageItemsById: {},
             annotationsByImage: {},
             superpixelsToTrain: [],
-            selectedIndex: 0,
             currentImageMetadata: {},
-            map: null
+            map: null,
+            page: 0,
+            selectedImageId: this.sortedSuperpixelIndices[0].imageId
         };
     },
     computed: {
         superpixelsToDisplay() {
-            return this.sortedSuperpixelIndices.slice(0, 8);
+            const startIndex = this.page * 8;
+            const endIndex = Math.min(startIndex + 8, this.sortedSuperpixelIndices.length);
+            return this.sortedSuperpixelIndices.slice(startIndex, endIndex);
         },
-        selectedImageId() {
-            return this.superpixelsToDisplay[0].imageId;
+        selectedIndex() {
+            return store.selectedIndex;
         }
     },
     methods: {
+        updateMapBaseImage() {
+            if (this.map && this.map.exit) {
+                this.map.exit();
+            }
+            restRequest({
+                url: `item/${this.selectedImageId}/tiles`
+            }).done((resp) => {
+                this.currentImageMetadata = resp;
+                const params = geo.util.pixelCoordinateParams(
+                    this.$refs.map,
+                    resp.sizeX,
+                    resp.sizeY,
+                    resp.tileWidth,
+                    resp.tileHeight
+                );
+                this.map = geo.map(params.map);
+                params.layer.url = `${this.apiRoot}/item/${this.selectedImageId}/tiles/zxy/{z}/{x}/{y}`;
+                this.map.createLayer('osm', params.layer);
+                // add layer for annotation
+                this.updateMapBoundsForSelection();
+            });
+        },
+        drawPixelmapAnnotation() {
+            console.log('drawing pixelmap annotation...');
+        },
+        updateMapBoundsForSelection() {
+            console.log('updating map bounds for selection...');
+            const superpixel = this.superpixelsToDisplay[this.selectedIndex];
+            const bbox = superpixel.bbox;
+            const bboxWidth = bbox[2] - bbox[0];
+            const bboxHeight = bbox[3] - bbox[1];
+            const scaleX = Math.abs((2 * bboxWidth) / this.currentImageMetadata.sizeX);
+            const scaleY = Math.abs((2 * bboxHeight) / this.currentImageMetadata.sizeY);
+            const zoom = this.map.zoom() - Math.log2(Math.max(scaleX, scaleY));
+            const center = {
+                x: (bbox[0] + bbox[2]) / 2,
+                y: (bbox[1] + bbox[3]) / 2
+            };
+            console.log({map: this.map, zoom, center, scaleX, scaleY, bboxWidth, bboxHeight});
+            this.map.zoom(zoom);
+            this.map.center(center);
+        }
     },
     watch: {
-        selectedIndex(newValue) {
+        selectedIndex() {
             // clear and update the map
-        },
-        currentImageMetadata(newValue) {
-            console.log(newValue);
-            this.map.exit();
-            const params = geo.util.pixelCoordinateParams(
-                this.$refs.map,
-                newValue.sizeX,
-                newValue.sizeY,
-                newValue.tileWidth,
-                newValue.tileHeight
-            );
-            this.map = geo.map(params.map);
-            params.layer.url = `${this.apiRoot}/item/${this.selectedImageId}/tiles/zxy/{z}/{x}/{y}`;
-            this.map.createLayer('osm', params.layer);
+            console.log('selected index changed');
+            // see if we need to update which image is displayed
+            const newImageId = this.superpixelsToDisplay[this.selectedIndex].imageId;
+            if (newImageId !== this.currentImageId) {
+                this.updateMapBaseImage();
+            } else {
+                // same base image, different selection
+                this.updateMapBoundsForSelection();
+            }
+            // calculate which bounds to set
+            // draw a new bounding box?
         }
     },
     mounted() {
-        console.log('vue component mounted...');
-        const mapContainer = this.$refs.map;
-        const map = geo.map({ node: mapContainer });
-        this.map = map;
-        restRequest({
-            url: `item/${this.selectedImageId}/tiles`
-        }).done((resp) => {
-            this.currentImageMetadata = resp;
-        });
+        store.apiRoot = this.apiRoot;
+        this.updateMapBaseImage();
     }
 });
 </script>
@@ -78,6 +115,7 @@ export default Vue.extend({
         <active-learning-film-strip
             :superpixelsToDisplay="this.superpixelsToDisplay"
             :apiRoot="this.apiRoot"
+            :selectedIndex="this.selectedIndex"
         />
     </div>
 </template>
