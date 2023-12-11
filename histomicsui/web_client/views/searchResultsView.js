313f0ad0b5fa45d98ae9d5e187fa8c3003501dc7
David Manthey <david.manthey@kitware.com>
2023-12-11 11:03:24 -0500

    Show resource paths in girder search pages.
    If you have multiple resources with the same name, the search page
    wasn't useful.

---
+++ histomicsui/web_client/views/searchResultsView.js
@@ -0,0 +1,49 @@
+import $ from 'jquery';
+
+import {wrap} from '@girder/core/utilities/PluginUtils';
+import {restRequest} from '@girder/core/rest';
+import SearchResultsView from '@girder/core/views/body/SearchResultsView';
+
+import '../stylesheets/views/searchResultsView.styl';
+
+function addResourcePaths(v) {
+    if (!v._results || !v._results.length) {
+        return;
+    }
+    const payload = {};
+    payload[v._type] = v._results.map((r) => r._id);
+    const resources = JSON.stringify(payload);
+    restRequest({
+        type: 'POST',
+        contentType: 'application/json',
+        processData: false,
+        url: 'resource/paths',
+        error: null,
+        data: resources
+    }).done((resp) => {
+        const payload = {};
+        payload[v._type] = v._results.map((r) => r._id);
+        if (resources !== JSON.stringify(payload)) {
+            return;
+        }
+        console.log(payload, resp);
+        v._results.forEach((doc, idx) => {
+            $(v.$el.find('.g-search-result')[idx]).append($('<span class="g-hui-search-result-resource-path">').text(resp[v._type][doc._id]));
+        });
+    });
+}
+
+wrap(SearchResultsView, 'render', function (render) {
+    render.call(this);
+    this._request.done(() => {
+        Object.values(this._subviews).forEach((v) => {
+            v._origrender = v.render;
+            v.render = function () {
+                v._origrender();
+                addResourcePaths(v);
+                return v;
+            };
+            addResourcePaths(v);
+        });
+    });
+});
