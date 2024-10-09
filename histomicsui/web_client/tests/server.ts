0579336a621c79abae7afa5ef5072d35f0a0d96a
Zach Mullen <zach.mullen@kitware.com>
2024-10-09 19:25:49 -0400

    Support playwright test recording

--- histomicsui/web_client/tests/server.ts
+++ histomicsui/web_client/tests/server.ts
@@ -1,16 +1,39 @@
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 import { expect, test } from '@playwright/test';
 
-const mongoUri = process.env.GIRDER_CLIENT_TESTING_MONGO_URI ?? 'mongodb://localhost:27017';
+const mongoUri = process.env.GIRDER_CLIENT_TESTING_MONGO_URI ?? 'mongodb://mongodb:27017';
 
 const startServer = async (port: number) => {
   const database = `${mongoUri}/girder-${port}`;
-  const serverProcess = spawn('girder', [
-    'serve',
-    '--database', database,
-    '--port', `${port}`,
-    '--with-temp-assetstore',
-  ], {
+  let command, args;
+
+  if (process.env.HISTOMICS_PLAYWRIGHT_DEV) {
+    // For recording with playwright, it's run through VSCode and Girder is run via docker-compose
+    command = 'docker-compose';
+    args = [
+        'run',
+        '-p', `${port}:${port}`,
+        '--rm',
+        'histomicsui',
+        'girder',
+        'serve',
+        '--host', '0.0.0.0',
+        '--with-temp-assetstore',
+        '--port', `${port}`,
+        '--database', database,
+      ];
+  } else {
+    // For running tests in CI, this process is run inside of the histomicsui docker container
+    command = 'girder';
+    args = [
+      'serve',
+      '--database', database,
+      '--port', `${port}`,
+      '--with-temp-assetstore',
+    ];
+  }
+
+  const serverProcess = spawn(command, args, {
     env: {
       ...process.env,
       GIRDER_SETTING_CORE_CORS_ALLOW_ORIGIN: '*',
@@ -36,7 +59,7 @@ const startServer = async (port: number) => {
 
 export const setupServer = () => {
   let serverProcess: ChildProcessWithoutNullStreams;
-  const port = Math.floor(Math.random() * 1000 + 6000);
+  const port = Math.floor(Math.random() * 2000 + 6000);
 
   test.beforeAll(async () => {
     serverProcess = await startServer(port);
@@ -74,7 +97,7 @@ export const setupServer = () => {
   });
 
   test.beforeEach(async ({ page }) => {
-    await page.goto(`http://localhost:${port}/`);
+    await page.goto(`http://0.0.0.0:${port}/`);
     await expect(page.getByRole('link', { name: 'About' })).toBeVisible();
   });
 };
