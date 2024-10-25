9fb3cdde5a770972b0020c3ca9f6d0389dcef465
David Manthey <david.manthey@kitware.com>
2024-10-25 10:13:08 -0400

    Add a pyproject.toml base to reduce pip 24.2 warnings

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     install_requires=[
         'girder-large-image-annotation>=1.30.1',
