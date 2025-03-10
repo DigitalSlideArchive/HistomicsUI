117eaf63d4b3617711579725a95e43ab50dd7b75
David Manthey <david.manthey@kitware.com>
2025-03-10 08:43:02 -0400

    Make sure style groups can't hold invalid keys

--- histomicsui/web_client/models/StyleModel.js
+++ histomicsui/web_client/models/StyleModel.js
@@ -1,10 +1,37 @@
+import _ from 'underscore';
 import Backbone from 'backbone';
+
+const AllowedKeys = ['fillColor', 'lineColor', 'lineWidth', 'label', 'group', 'id'];
 
 const StyleModel = Backbone.Model.extend({
     defaults: {
         lineWidth: 2,
         lineColor: 'rgb(0,0,0)',
         fillColor: 'rgba(0,0,0,0)'
+    },
+    initialize: function (attributes, options) {
+        if (attributes) {
+            attributes = Object.assign({}, attributes);
+            Object.keys(attributes).forEach((k) => {
+                if (!AllowedKeys.includes(k)) {
+                    delete attributes[k];
+                }
+            });
+        }
+        Backbone.Model.prototype.initialize.call(this, attributes, options);
+    },
+    set: function (key, value, options) {
+        if (_.isObject(key) && !_.isFunction(key)) {
+            key = Object.assign({}, key);
+            Object.keys(key).forEach((k) => {
+                if (!AllowedKeys.includes(k)) {
+                    delete key[k];
+                }
+            });
+            Backbone.Model.prototype.set.call(this, key, value, options);
+        } else if (AllowedKeys.includes(key)) {
+            Backbone.Model.prototype.set.call(this, key, value, options);
+        }
     }
 });
 
